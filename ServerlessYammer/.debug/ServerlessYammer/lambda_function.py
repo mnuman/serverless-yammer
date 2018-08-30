bafc30413ebf0630e949e6a4a9c3ad9fdd06e5da
Milco Numan <milco.numan@gmail.com>
2018-08-30 12:59:16 +0000

    Lets Yammer!

--- ServerlessYammer/.debug/ServerlessYammer/lambda_function.py
+++ ServerlessYammer/.debug/ServerlessYammer/lambda_function.py
@@ -1,8 +1,15 @@
 import json
 import requests
 def lambda_handler(event, context):
+    print(json.dumps(event))
+    
     body = json.loads(event['body'])
     myMessage = f"Hi, {body['head_commit']['author']['name']} has just committed code at {body['head_commit']['url']} with message {body['head_commit']['message']}"
+    auth = { "Authorization" : "Bearer 10691656-iLf4mZMqSZzQlnAKeQq4aA"}
+    payload = { "data" : myMessage, "group_id" : 15767042} 
+    r = requests.post("https://www.yammer.com/api/v1/messages.json", headers=auth, data=payload)
+
+    print("Status code Yammer call:" + r.status_code)
     
     return {
         "isBase64Encoded" : "false",
