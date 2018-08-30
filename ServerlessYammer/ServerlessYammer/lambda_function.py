543acd20e0694f95f2871a2f4ebb713001389a6d
Milco Numan <milco.numan@gmail.com>
2018-08-30 12:26:25 +0000

    Formatting a proper message for Yammer, but send this only back in the response for now ...

--- ServerlessYammer/ServerlessYammer/lambda_function.py
+++ ServerlessYammer/ServerlessYammer/lambda_function.py
@@ -1,3 +1,12 @@
+import json
+import requests
 def lambda_handler(event, context):
-    # TODO implement
-    return ''
+    body = json.loads(event['body'])
+    myMessage = f"Hi, {body['head_commit']['author']['name']} has just committed code at {body['head_commit']['url']} with message {body['head_commit']['message']}"
+    
+    return {
+        "isBase64Encoded" : "false",
+        "statusCode" : 200,
+        "headers" : {},
+        "body" : json.dumps(myMessage)
+    }
