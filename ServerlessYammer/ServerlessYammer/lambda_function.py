9d5234ab9d8e23c01fd7bbd06214a1e608ff4992
Milco Numan <milco.numan@gmail.com>
2018-08-30 13:20:52 +0000

    Invalidated my developer token and generated a new one; pushed the token into a environment variable set MANUALLY on the Lambda function after deployment

--- ServerlessYammer/ServerlessYammer/lambda_function.py
+++ ServerlessYammer/ServerlessYammer/lambda_function.py
@@ -1,11 +1,12 @@
 import json
+import os
 import requests
 def lambda_handler(event, context):
     print(json.dumps(event))
     
     body = json.loads(event['body'])
     myMessage = f"Hi, {body['head_commit']['author']['name']} has just committed code at {body['head_commit']['url']} with message {body['head_commit']['message']}"
-    auth = { "Authorization" : "Bearer 10691656-iLf4mZMqSZzQlnAKeQq4aA"}
+    auth = { "Authorization" : "Bearer " + os.environ['YAMMER_KEY']}
     payload = { "body" : myMessage, "group_id" : 15767042} 
     r = requests.post("https://www.yammer.com/api/v1/messages.json", headers=auth, data=payload)
 
